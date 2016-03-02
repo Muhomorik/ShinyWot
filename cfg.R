112501e0d8512c945e898cc0a27696c53bb18296
Dmitri Peredera <dmitri.peredera@gmail.com>
2016-03-02 14:36:28 +0300

    re-commit

---
+++ cfg.R
@@ -0,0 +1,14 @@
+# Don't source this file to GitHub with real creds!
+
+GetWgApiKey <- function(){
+  # Get Wg API private key. Or use 'demo'.
+  # 
+  # Returns:
+  #   Sected key from account page.
+  
+  message("GetWgApiKey.")
+  
+  key <- "demo"  
+  key
+}
+
